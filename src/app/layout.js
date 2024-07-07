5b78a500aca5735e2064fba8a6f3f5c105437cc1
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-07-07 19:33:23 +0300

    start-header-template

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <p>Selamlarr</p>
+        <Header />
         {children}
       </body>
     </html>
